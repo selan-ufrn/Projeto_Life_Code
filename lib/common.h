e52ef3140b71b9dbdbb7bd81d9998010ae9b0c95
Alv <93353239+alvsoares1@users.noreply.github.com>
2022-11-19 19:12:32 -0300

    Common

--- lib/common.h
+++ lib/common.h
@@ -1,87 +1,180 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-#include <cstring>  // memset, memcpy
+#include <cstring>
 #include <iostream>
 #include <map>
 
 namespace life {
-/// Represents a Color as a RGB entity.
-struct Color {
-  //=== Alias
-  typedef unsigned char color_t;  //!< Type of a color channel.
-  /// Identifies each color channel.
-  enum channel_e : color_t {
-    R = 0,  //!< Red channel.
-    G = 1,  //!< Green channel.
-    B = 2   //!< Blue channel
-  };
+   // Coordenadas do pixel (x,y)
+   struct Coordinates
+   {
+       typedef long coord_type; // Tipo da coordenada
+       coord_type x; // Ponto x
+       coord_type y; // Ponto y
+       // Cria o ponto da coordenada
+       Coordinates( coord_type x=0, coord_type y=0 ) : x{x}, y{y} {/*empty*/}
+       // Construtor cópia
+       Coordinates( const Coordinates& clone ) : x{ clone.x }, y{ clone.y } {/*empty*/}
+       // Operador de comparação
+       bool operator==( const Coordinates& rhs ) const
+       { return x==rhs.x and y==rhs.y ; }
+       // Operador de comparação de diferença
+       bool operator!=( const Coordinates& rhs ) const
+       { return not operator==( rhs ) ; }
+       // Decrementador
+       Coordinates operator--()
+       {
+           --x; --y;
+           return *this;
+       }
+       // Decrementa apos a operação
+       Coordinates operator--(int)
+       {
+           Coordinates temp(*this);
+           x--; y--;
+           return temp;
+       }
+       /// Operador de streams
+       friend std::ostream& operator<<( std::ostream& os, const Coordinates& p )
+       {
+           os << "(x="<< p.x << ", y=" << p.y << ")";
+           return os;
+       }
+   };
 
-  //=== Members
-  color_t channels[3];  //!< Stores each of the color channels, R, G, and B.
-  //=== Methods
-  /// Creates a color.
-  Color(color_t r = 0, color_t g = 0, color_t b = 0) : channels{ r, g, b }
-  { /*empty*/
-  }
-  /// Copy constructor.
-  Color(const Color& clone)
-  {
-    std::memcpy(channels, clone.channels, sizeof(color_t) * 3);
-  }
-  /// Assignment operator.
-  Color& operator=(const Color& rhs)
-  {
-    if (&rhs != this)
-      std::memcpy(channels, rhs.channels, sizeof(color_t) * 3);
-    return *this;
-  }
-  /// Comparison operator.
-  bool operator==(const Color& rhs)
-  {
-    return not std::memcmp(channels, rhs.channels, sizeof(color_t) * 3);
-  }
-  /// Prints out a string representation of a color to an output stream.
-  friend std::ostream& operator<<(std::ostream& os, const Color& c)
-  {
-    os << "(" << (int)c.channels[0] << "," << (int)c.channels[1] << "," << (int)c.channels[2]
-       << ")";
-    return os;
-  }
-};
+   /// Representa a coordenada na matrix
+   struct Matrix
+   {
+       typedef long coord_type;
+       coord_type r; // linha
+       coord_type c; // coluna
+       // Cria a localização na matriz
+       Matrix( coord_type r=0, coord_type c=0 ) : r{r}, c{c} {/*empty*/}
+       // Construtor cópia
+       Matrix( const Matrix& clone ) : r{ clone.r }, c{ clone.c } {/*empty*/}
+       // Comparador de igualdade
+       bool operator==( const Matrix& rhs ) const
+       { return r==rhs.r and c==rhs.c ; }
+       // Comparador de diferença
+       bool operator!=( const Matrix& rhs ) const
+       { return not operator==( rhs ) ; }
+       bool operator<( const Matrix& rhs ) const
+       { return (r < rhs.r or ( r == rhs.r and c < rhs.c ) ); }
+       // Decrementador anterior
+       Matrix operator--()
+       {
+           --r; --c;
+           return *this;
+       }
+       // Decrementador posterior
+       Matrix operator--(int)
+       {
+           Matrix temp(*this);
+           r--; c--;
+           return temp;
+       }
+       // Operador de stream
+       friend std::ostream& operator<<( std::ostream& os, const Matrix& l )
+       {
+           os << "(row="<< l.r << ", col=" << l.c << ")";
 
-// A basic color pallete.
-static const Color BLACK = Color{ 0, 0, 0 };               //!< Black.
-static const Color WHITE = Color{ 255, 255, 255 };         //!< White.
-static const Color DARK_GREEN = Color{ 0, 100, 0 };        //!< Dark green.
-static const Color GREEN = Color{ 0, 250, 0 };             //!< Green.
-static const Color RED = Color{ 255, 0, 0 };               //!< Red.
-static const Color BLUE = Color{ 0, 0, 255 };              //!< Blue.
-static const Color CRIMSON = Color{ 220, 20, 60 };         //!< Crimson (kind of red).
-static const Color LIGHT_BLUE = Color{ 135, 206, 250 };    //!< Light blue.
-static const Color LIGHT_GREY = Color{ 210, 210, 210 };    //!< Light blue.
-static const Color DEEP_SKY_BLUE = Color{ 0, 191, 255 };   //!< Deep blue.
-static const Color DODGER_BLUE = Color{ 30, 144, 255 };    //!< Another bluish color.
-static const Color STEEL_BLUE = Color{ 70, 130, 180 };     //!< Yet another bluish color.
-static const Color YELLOW = Color{ 255, 255, 0 };          //!< Yellow.
-static const Color LIGHT_YELLOW = Color{ 255, 255, 153 };  //!< Light yellow.
+           return os;
+       }
+   };
 
-/// A color palette for later use.
-static std::map<std::string, Color> color_pallet{ { "black", BLACK },
-                                                  { "white", WHITE },
-                                                  { "dark_green", DARK_GREEN },
-                                                  { "red", RED },
-                                                  { "green", GREEN },
-                                                  { "blue", BLUE },
-                                                  { "crimson", CRIMSON },
-                                                  { "light_blue", LIGHT_BLUE },
-                                                  { "light_grey", LIGHT_GREY },
-                                                  { "deep_sky_blue", DEEP_SKY_BLUE },
-                                                  { "dodger_blue", DODGER_BLUE },
-                                                  { "steel_blue", STEEL_BLUE },
-                                                  { "yellow", YELLOW },
-                                                  { "light_yellow", LIGHT_YELLOW } };
+   // Converte localização na matrix para um ponto de coordenada
+   inline Coordinates CoordinateToMatrix( const Matrix &l )
+   { return Coordinates{ l.c, l.r }; }
 
-}  // namespace life
+   // Converte coordenada para localização na matriz
+   inline Matrix MatrixToCoordinate( const Coordinates &p )
+   { return Matrix{ p.y, p.x }; }
 
-#endif  // COMMON_H
+   // Representa a cor no RGB
+   struct Color
+   {
+
+       typedef unsigned char color_t; 
+
+       enum channel_e : color_t { R=0, 
+                                  G=1, 
+                                  B=2, 
+                                  A=3  
+       };  
+
+       static constexpr short N_CHANNELS = 4;
+
+    
+       color_t channels[N_CHANNELS]; 
+ 
+       // Construtor da cor
+       Color(color_t r=0, color_t g=0, color_t b=0, color_t a=0) : channels{r,g,b,a} {/*empty*/}
+
+       // Construtor cópia
+       Color( const Color& clone )
+       {
+           std::memcpy( channels, clone.channels, sizeof(color_t)*N_CHANNELS );
+       }
+
+       // Operador de atribuição
+       Color& operator = ( const Color& rhs )
+       {
+           if ( &rhs != this )
+               std::memcpy( channels, rhs.channels, sizeof(color_t)*N_CHANNELS );
+           return *this;
+       }
+
+       // Operador de comparação
+       bool operator == ( const Color& rhs )
+       {
+           return not std::memcmp( channels, rhs.channels, sizeof(color_t)*N_CHANNELS );
+       }
+       
+       friend std::ostream& operator<<( std::ostream& os, const Color& c )
+       {
+           os << "(" << (int)c.channels[R] << ","
+                     << (int)c.channels[G] << ","
+                     << (int)c.channels[B] << ","
+                     << (int)c.channels[A] << ")";
+           return os;
+       }
+   };
+
+ // Paleta de cores
+   static const Color BLACK         = Color{0,0,0, 255}      ; 
+   static const Color WHITE         = Color{255,255,255, 255}; 
+   static const Color DARK_GREEN    = Color{0,100,0, 255}    ; 
+   static const Color GREEN         = Color{0,250,0, 255}    ; 
+   static const Color RED           = Color{255,0,0, 255}    ; 
+   static const Color BLUE          = Color{0,0,255, 255}    ; 
+   static const Color CRIMSON       = Color{220,20,60, 255}  ; 
+   static const Color LIGHT_BLUE    = Color{135,206,250, 255}; 
+   static const Color LIGHT_GREY    = Color{210,210,210, 255}; 
+   static const Color DEEP_SKY_BLUE = Color{0,191,255, 255}  ; 
+   static const Color DODGER_BLUE   = Color{30,144,255, 255} ; 
+   static const Color STEEL_BLUE    = Color{70,130,180, 255} ; 
+   static const Color YELLOW        = Color{255,255,0, 255}  ; 
+   static const Color LIGHT_YELLOW  = Color{255,255,153, 255}; 
+
+   /// Paleta de cores
+   static std::map< std::string, Color > color_pallet{
+       {  "black",         BLACK },
+       {  "white",         WHITE },
+       {  "dark green",    DARK_GREEN },
+       {  "red",           RED },
+       {  "green",         GREEN },
+       {  "blue",          BLUE },
+       {  "crimson",       CRIMSON },
+       {  "light blue",    LIGHT_BLUE },
+       {  "light grey",    LIGHT_GREY },
+       {  "deep sky blue", DEEP_SKY_BLUE },
+       {  "dodger blue",   DODGER_BLUE },
+       {  "steel blue",    STEEL_BLUE },
+       {  "yellow",        YELLOW },
+       {  "light yellow",  LIGHT_YELLOW }
+   };
+
+} // namespace
+
+#endif // COMMON_H
